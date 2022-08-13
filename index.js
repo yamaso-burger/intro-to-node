0a2198e52219538729330334aa87e0d3707ac23f
yamaso-burger <apple_1228_ringo@yahoo.co.jp>
2022-08-13 10:59:37 +0900

    install JSON files using npm

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-// The Node RemotePlayback(Read Evaluation Print Loops)
 
-// Flie system module. It enables us to read and write on local files
-const fs = require("fs");
+var superheroes = require("superheroes");
+var supervillains = require("supervillains");
 
-fs.copyFileSync("file.txt", "file2.txt");
+var mySuperheroName = superheroes.random();
+var mySupervillainName = supervillains.random();
+
+console.log(mySuperheroName + " vs. " + mySupervillainName);
