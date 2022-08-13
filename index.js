6ca9d760556a832783fa66f2d6dedfe94fe6a7e8
yamaso-burger <apple_1228_ringo@yahoo.co.jp>
2022-08-13 10:48:49 +0900

    initialize NPM Package Managaer

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-hello
+// The Node RemotePlayback(Read Evaluation Print Loops)
+
+// Flie system module. It enables us to read and write on local files
+const fs = require("fs");
+
+fs.copyFileSync("file.txt", "file2.txt");
